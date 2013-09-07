3b2025d8fa07b83ea653cd83c57f7fea855cc3e3
Christian Lischnig <lischc@users.noreply.github.com>
2013-09-07 20:06:15 +0000

    initial import

---
+++ CongressDymoLabelApp/SocketHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Net;
+using System.IO;
+using System.Web;
+
+namespace CongressDymoLabelApp
+{
+    class SocketHelper
+    {
+        TcpClient mscClient;
+        string mstrMessage;
+        // string mstrResponse;
+        // byte[] bytesSent;
+        public bool processMsg(TcpClient client, NetworkStream stream, byte[] bytesReceived, MainForm mainForm)
+        {
+            // Handle the message received and  
+            // send a response back to the client.
+            mstrMessage = Encoding.UTF8.GetString(bytesReceived, 0, bytesReceived.Length);
+            Debug.WriteLine("mstrMessage: " + mstrMessage);
+            mscClient = client;
+            String control = mstrMessage.Substring(0, 4);
+            if (control.Equals("STOP"))
+            {
+                return true;
+            }
+            else
+            {
+                // int indexstart = mstrMessage.IndexOf(" HTTP/1.1");
+                // String firstline = mstrMessage.Remove(indexstart).Replace("GET /?", "");
+                String firstline = mstrMessage.Replace("GET /?", "");
+                firstline = Uri.UnescapeDataString(firstline);
+                // firstline = firstline.Remove(firstline.IndexOf("&end=true"));
+                String[] parts = firstline.Split(new String[] { "&" }, StringSplitOptions.RemoveEmptyEntries);
+                String firstname = "";
+                String lastname = "";
+                String lectures = "";
+                String seminars = "";
+                foreach (String part in parts)
+                {
+                    Debug.WriteLine("parts=" + parts);
+                    String[] kv = part.Split(new String[] { "=" }, StringSplitOptions.RemoveEmptyEntries);
+                    if ("firstname".Equals(kv[0]))
+                    {
+                        firstname = kv.Length > 1 ? kv[1] : "";
+                    } 
+                    else if ("lastname".Equals(kv[0]))
+                    {
+                        lastname = kv.Length > 1 ? kv[1] : "";
+                    } 
+                    else if ("lectures".Equals(kv[0]))
+                    {
+                        lectures = kv.Length > 1 ? kv[1] : "";
+                    }
+                    else if ("seminars".Equals(kv[0]))
+                    {
+                        seminars = kv.Length > 1 ? kv[1] : "";
+                    }
+                }
+                mainForm.addAndPrint(firstname, lastname, lectures, seminars);
+                return false;
+            }
+        }
+
+        private void BuildReqStream(ref WebRequest webrequest, String content)
+        //This method build the request stream for WebRequest
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(content);
+            webrequest.ContentLength = bytes.Length;
+            try
+            {
+                Stream oStreamOut = webrequest.GetRequestStream();
+                oStreamOut.Write(bytes, 0, bytes.Length);
+                oStreamOut.Close();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ CongressDymoLabelApp/Worker.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+namespace CongressDymoLabelApp
+{
+    public class Worker
+    {
+        private volatile bool _shouldStop;
+        volatile static string output = "";
+        private volatile MainForm mainForm;
+
+        public Worker(MainForm mainForm)
+        {
+            this.mainForm = mainForm;
+        }
+        
+        public void doWork()
+        {
+            TcpListener tcpListener = null;
+            try
+            {
+                // Set the listener on the local IP address 
+                // and specify the port.
+                tcpListener = new TcpListener(MainForm.selectedAddress, 9500);
+                tcpListener.Start();
+                output = "Waiting for a connection...";
+            }
+            catch (Exception e)
+            {
+                output = "Error: " + e.ToString();
+                MessageBox.Show(output);
+            }
+            while (!this._shouldStop)
+            {
+                // Always use a Sleep call in a while(true) loop 
+                // to avoid locking up your CPU.
+                Thread.Sleep(10);
+                // Create a TCP socket. 
+                // If you ran this server on the desktop, you could use 
+                // Socket socket = tcpListener.AcceptSocket() 
+                // for greater flexibility.
+                Debug.WriteLine("Waiting for connection...");
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                Debug.WriteLine("Connected");
+                // Read the data stream from the client. 
+                byte[] bytes = new byte[256];
+                NetworkStream stream = tcpClient.GetStream();
+                stream.Read(bytes, 0, bytes.Length);
+                SocketHelper helper = new SocketHelper();
+                this._shouldStop = helper.processMsg(tcpClient, stream, bytes, this.mainForm);
+                tcpClient.Close();
+            }
+        }
+    }
+}
